13cfed575f48ac2fb66cb90166af70451640a285
Jason Lo <mr.lo.mhs@gmail.com>
2014-03-25 16:34:19 -0700

    oop

--- yelp.rb
+++ yelp.rb
@@ -5,16 +5,38 @@ require 'awesome_print'
 require 'json'
 
 
-consumer_key = ''
-consumer_secret = ''
-token = ''
-token_secret = ''
 
-api_host = 'api.yelp.com'
+# api_host = 'api.yelp.com'
 
-consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+# consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+# access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
-path = "/v2/search?term=bars&location=San+Francisco&limit=1"
+# path = "/v2/search?term=bars&location=San+Francisco&limit=1"
 
-ap JSON.parse(access_token.get(path).body)
+# ap JSON.parse(access_token.get(path).body)
+
+class Yelp
+  def initialize
+    consumer_key = ''
+    consumer_secret = ''
+    token =''
+    token_secret = ''
+    api_host = 'api.yelp.com'
+    consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+    @access_token = OAuth::AccessToken.new(consumer, token, token_secret)
+  end
+
+  def search(options={})
+    term = options[:term]
+    location = options[:location].split.join("+")
+    limit = options[:limit] || 5
+    path = "/v2/search?term=#{term}&location=#{location}&limit=#{limit}"
+    JSON.parse(@access_token.get(path).body)["businesses"].map{|business| business["name"]}
+  end
+
+
+end
+
+yelp = Yelp.new
+ap yelp.search({term: "bars", location: "San Francisco", limit: 10})
+ap yelp.search({term: "restaurants", location: "Mountain View", limit: 10})
